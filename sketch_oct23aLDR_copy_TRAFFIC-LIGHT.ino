50c6a524e59d0c43bd7195280b6eac86c6cd65d5
SANTANU KOUSHIK BORGOHAIN <santanukoushik04@gmail.com>
2024-11-22 19:52:05 +0530

    Add files via upload

---
+++ sketch_oct23aLDR_copy_TRAFFIC-LIGHT.ino
@@ -0,0 +1,34 @@
+#define PIN_RED D1 
+#define PIN_YELLOW D2
+#define PIN_GREEN D3
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN_RED, OUTPUT);
+  pinMode(PIN_YELLOW, OUTPUT);
+  pinMode(PIN_GREEN, OUTPUT);
+}
+
+void loop() {
+  // red light on
+  digitalWrite(PIN_RED, HIGH);
+  digitalWrite(PIN_YELLOW, LOW);
+  digitalWrite(PIN_GREEN, LOW);  
+  delay(100);
+
+  // yellow light on
+  digitalWrite(PIN_RED, LOW);    
+  digitalWrite(PIN_YELLOW, HIGH);
+  digitalWrite(PIN_GREEN, LOW); 
+  delay(100); 
+
+  // green light on
+  digitalWrite(PIN_RED, LOW);    
+  digitalWrite(PIN_YELLOW, LOW); 
+  digitalWrite(PIN_GREEN, HIGH);
+  delay(100); 
+}
